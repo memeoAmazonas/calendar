ea8af6503e56853216e332f38822ee0b4fc2f083
Jose Ortiz <jortiz@bitdharma.com>
2018-07-13 16:55:05 -0400

    calendar example

---
+++ src/component/item.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Item extends React.PureComponent {
+   result = () => {
+
+   }
+render() {
+  return(
+    <div>
+    {this.result}
+    </div>
+  );
+}
+
+}
+
+export default Item;
+
--- src/component/js/calendar.js
+++ src/component/js/calendar.js
@@ -7,23 +7,43 @@ import  '../css/calendar.css';
 
 
 class Calendar extends React.PureComponent {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      initDate: '',
+      dayRender: 0,
+      countryCode: ''
+    };
+  }
+  onchange = (e) => {
+    const {name, value} = e.target;
+    this.setState({ [name]: value })
+  }
 
   render(){
     return(
       <div className="calendar-container">
-        <div className="input-container">
-          <span className="title-input">{strings.labels.initDate}</span>
+        <div className="container- labels">
+          <div className="input-container">
+            <span className="title-input">{strings.labels.initDate}</span>
 
-          <input type="date" />
+            <input type="date" name="initDate" formMethod="dd/MM/YYYY" onChange={(e)=>{this.onchange(e)}} />
+          </div>
+          <div className="input-container">
+            <span className="title-input">{strings.labels.dayRender}</span>
+            <input type="number" name="dayRender" min={0} onChange={(e)=>{this.onchange(e)}} />
+          </div>
+          <div className="input-container">
+            <span className="title-input-date">{strings.labels.countryCode}</span>
+            <input type="text" name="countryCode" onChange={(e)=>{this.onchange(e)}} />
+          </div>
         </div>
-        <div className="input-container">
-          <span className="title-input">{strings.labels.dayRender}</span>
-          <input type="number" />
-        </div>
-        <div className="input-container">
-          <span className="title-input-date">{strings.labels.countryCode}</span>
-          <input type="text" />
+        <div className="container-result">
+          <span>{this.state.initDate}</span>
+          <br/>
+          <span>{this.state.dayRender}</span>
+          <br/>
+          <span>{this.state.countryCode}</span>
         </div>
       </div>
     );
